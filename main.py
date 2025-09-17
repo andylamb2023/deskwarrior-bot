0a96b2cf14fc80adabfe17306ca8134cd8d78ee5
andylamb2023 <132908110+andylamb2023@users.noreply.github.com>
2025-09-17 13:23:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
 API_TOKEN = "YOUR_TELEGRAM_BOT_API_KEY"
-ADMIN_HANDLE = "@YourTelegramHandle"   # who gets pinged
-WALLET_ADDRESS = "YOUR_USDT_WALLET_ADDRESS"
+ADMIN_HANDLE = @thebutchersblock   # who gets pinged
+WALLET_ADDRESS = testaddress
 
 bot = Bot(token=API_TOKEN, parse_mode="Markdown")
 dp = Dispatcher(bot)
